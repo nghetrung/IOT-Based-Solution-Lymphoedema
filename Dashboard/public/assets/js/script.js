429d345f1e057b4cb46776f9621563f1ba461212
Marco Lam <marcolam053@gmail.com>
2017-09-21 23:01:49 +1000

    Real time update chart fix BY Arjun & Trung

--- Dashboard/public/assets/js/script.js
+++ Dashboard/public/assets/js/script.js
@@ -1,5 +1,6 @@
 var database;
 var array = [];
+
 function setup() {
 
   var config = {
@@ -14,16 +15,16 @@ function setup() {
   database = firebase.database();
 
    createCanvas(0,0);
-
+   
   // Start loading the data
   loadFirebase();
 
 }
 
 function loadFirebase() {
-
+	 
   var ref = database.ref('Stanley');
-
+ 
   ref.child('affected').on("value", gotData, errData);
   /*var data=
   {
@@ -45,9 +46,6 @@ function errData(error) {
 // The data comes back as an object
 function gotData(data) {
 
-  //var listings = selectAll('.listing');
-  //clearList(listings);
-
   var scores=data.val();
   var keys=Object.keys(scores);
 
@@ -61,16 +59,16 @@ function gotData(data) {
 
   for(var i=0;i<keys.length;i++)
   {
-
+			
 			var k=keys[i];
 			//var name=scores[k].name;
 			var wrist=scores[k].wrist;
-			var Ten_cm=scores[k].tencm_reading;
-			var Twenty_cm=scores[k].twentycm_reading;
-			var Thirty_cm=scores[k].thirtycm_reading;
-			var Forty_cm=scores[k].fortycm_reading;
-
-
+			var tencm=scores[k].tencm_reading;
+			var twentycm=scores[k].twentycm_reading;
+			var thirtycm=scores[k].thirtycm_reading;
+			var fortycm=scores[k].fortycm_reading;
+			
+			
 			//var li1=createElement('li','Name '+name);
 			//li1.parent(listname);
 			//var li=createElement('li',' Tencm : '+tencm+' Twentycm : '+twentycm+' Thirtycm : '+thirtycm+' Fortycm : '+fortycm+' wrist :'+wrist);
@@ -78,54 +76,56 @@ function gotData(data) {
 			//li.class('listing');
 			//li.parent(list);
 
-			array.push(wrist,Ten_cm,Twenty_cm,Thirty_cm,Forty_cm);
+			array.push(wrist,tencm,twentycm,thirtycm,fortycm);
 			/*array.push(twentycm);
 			array.push(thirtycm);
 			array.push(fortycm);
 			array.push(wrist);
 			*/
-
-
-
+			
+			
+	
 
   }
   console.log(array);
   	// Chart js
 			// Our labels along the x-axis
-		var days = ["Monday","Tuesday","wednesday","Thursday","Friday","Saturday","Sunday"];
+		var days = ["Monday","Tuesday","wednesday","Thursday","Friday","Saturday"];
 			// For drawing the lines
 		var wristInitialIndex = 0;
 		var tenInitialIndex = 1;
 		var twentyInitialIndex = 2;
 		var thirtyInitialIndex = 3;
 		var fortyInitialIndex = 4;
-
-		var wrist = [];
+		
+		
+		var wrist1 = [];
 		for (var i = wristInitialIndex; i <= array.length - 5; i=i + 5) {
-			wrist.push(array[i]);
+			wrist1.push(array[i]);
+			
 		}
-
+		
 		var tencm = [];
 		for (var i = tenInitialIndex; i <= array.length - 4; i=i + 5) {
 			tencm.push(array[i]);
 		}
-
+		
 		var twentycm = [];
 		for (var i = twentyInitialIndex; i <= array.length - 3; i=i + 5) {
 			twentycm.push(array[i]);
 		}
-
+		
 		var thirtycm = [];
 		for (var i = thirtyInitialIndex; i <= array.length - 2; i=i + 5) {
 			thirtycm.push(array[i]);
 		}
-
+		
 		var fortycm = [];
 		for (var i = fortyInitialIndex; i <= array.length - 1; i=i + 5) {
 			fortycm.push(array[i]);
-		}
-
-
+		} 
+		
+		//console.log(wrist1);
 		var ctx = document.getElementById("myChart");
 
 		var myChart = new Chart(ctx, {
@@ -134,37 +134,37 @@ function gotData(data) {
 				labels: days,
 				datasets: [
 				{
-					data: wrist,
-					label : "Wrist",
-          backgroundColor : '	rgba(255,99,132,0.2)',
-					borderColor: 'rgba(255,99,132,0.5)',
+					data: wrist1,
+					label : "wristaj",
+					backgroundColor : 'rgba(255,99,132,0.2)',
+					borderColor: 'rgba(255,99,132,1)',
 					borderWidth : 1
 				},
 				{
 					data: tencm,
-					label : "10 cm",
-          backgroundColor : 'rgba(0,0,255,0.2)',
-          borderColor: 'rgba(0,0,255,0.5)',
+					label : "tenaj",
+					backgroundColor : 'rgba(255,99,132,0.2)',
+					borderColor: 'rgba(255,99,132,1)',
 					borderWidth : 1
 				},
 				{
 					data: twentycm,
-					label : "20 cm",
-          backgroundColor : 'rgba(0,255,0,0.2)',
-					borderColor: 'rgba(0,255,0,0.5)',
+					label : "twentyaj",
+					backgroundColor : 'rgba(255,99,132,0.2)',
+					borderColor: 'rgba(255,99,132,1)',
 					borderWidth : 1
 				},
 				{
 					data: thirtycm,
-					label : "30 cm",
-          backgroundColor : 'rgba(54,162,235,0.2)',
-          borderColor: 'rgba(54,162,235,1)',
+					label : "thirtyaj",
+					backgroundColor : 'rgba(255,99,132,0.2)',
+					borderColor: 'rgba(255,99,132,1)',
 					borderWidth : 1
 				},
 				{
 					data: fortycm,
-					label : "40 cm",
-          backgroundColor : 'rgba(255,99,132,0.2)',
+					label : "fortyaj",
+					backgroundColor : 'rgba(255,99,132,0.2)',
 					borderColor: 'rgba(255,99,132,1)',
 					borderWidth : 1
 				}
@@ -178,14 +178,23 @@ function gotData(data) {
   ]
   }
 });
-
+//console.log(wrist1.length);
+while(array.length>0)
+{
+	array.pop();
 }
 
 
-
-// Clear everything
-function clearList(listItems) {
-  for (var i = 0; i < listItems.length; i++) {
-    listItems[i].remove();
-  }
+ 
 }
+	
+
+
+
+ 
+
+
+
+
+
+
